739b7a6d32012d8b883b2ba152ad646116399792
gateszhangc <gateszhang92@gmail.com>
2025-11-26 17:23:42 +0800

    提交

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { readFileSync } from 'fs'
 import { join } from 'path'
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -19,7 +20,23 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head dangerouslySetInnerHTML={{ __html: headContent }} />
-      <body suppressHydrationWarning>{children}</body>
+      <body suppressHydrationWarning>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BLXZZ0WE9Y"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BLXZZ0WE9Y');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   )
 }

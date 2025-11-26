3101897146120ac61baac12a7c47c44eaf8053cb
gateszhangc <gateszhang92@gmail.com>
2025-11-26 17:31:48 +0800

    优化

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,16 @@ export default function RootLayout({
             gtag('config', 'G-DLWXX26WDV');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "uc2rxokr32");
+          `}
+        </Script>
         {children}
       </body>
     </html>
